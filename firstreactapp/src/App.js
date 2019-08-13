f02afd37ffd854bc765d36e12c4c3a4ed094ea1f
shyamprasad1997 <shyamprasad1997@gmail.com>
2019-08-13 09:40:55 +0530

    Completed first assignment

---
+++ firstreactapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!!</h1>
+      <img src="original.gif" alt="Error 404"></img>
+      <h1>This is my first react app!!</h1>
+    </div>
+  );
+}
+
+export default App;
