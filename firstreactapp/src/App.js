8a832871f92cdbf9c19fd8f0e18b71a028433b03
shyamprasad1997 <shyamprasad1997@gmail.com>
2019-08-13 15:52:27 +0530

    Completed second assignment

--- firstreactapp/src/App.js
+++ firstreactapp/src/App.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const feedback = 
+[ 
+{
+ name: 'Chris Martin',
+ email: 'chris@gmail.com',
+ feeedback: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took"},
+{
+ name: 'Jonas Peter',
+ email: 'jonas@gmail.com',
+ feeedback: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took"},
+{
+ name: 'Emi Stephen',
+ email: 'emi@gmail.com',
+ feeedback: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took"},
+{
+ name: 'Jack Paul',
+ email: 'jack@gmail.com',
+ feeedback: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took"}
+]
+
+
+class App extends React.Component {
+  render(){
   return (
-    <div className="App">
-      <h1>Hello World!!</h1>
-      <img src="original.gif" alt="Error 404"></img>
-      <h1>This is my first react app!!</h1>
-    </div>
-  );
+      <ul>
+        {feedback.map(item => (
+          <li className ="list">
+            <div className ="list1">
+            <div className="inside_list">
+              <div className="fas fa-address-card"></div>
+              <span className="text"><h3>Name: {item.name}<br></br> Email: {item.email}</h3><h4><strong>Feedback: </strong>{item.feeedback}</h4></span>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>    
+  );  
+  }
 }
 
 export default App;
